9edb7388f2c47bf446c82e10685bd71dbb8b4688
Nicolas Rovetta <nico.rovetta@gmail.com>
2023-01-10 15:00:20 -0300

    copi3

--- JS/function.js
+++ JS/function.js
@@ -1,6 +1,35 @@
-
-document.getElementById('boton').onclick = function () {
-    console.log("Hacer click aquí");
-    document.getElementById("demo").innerHTML = "Probando primer evento en JS";
+/**
+ document.getElementById('boton').onclick = function () {
+       console.log("Capturamos el momento click");
+       document.getElementById("demo").innerHTML = "Estamos probando evento en JS"; 
 }
 
+document.addEventListener('click', function(){
+       console.log("Hola mundo desde EventListener");
+       document.getElementById("demo").innerHTML = "Estamos probando evento en JS";
+        
+});
+*/
+
+document.getElementById('boton').addEventListener( 'click' , function () {
+    console.log("Hola mundo desde EventListener");
+    document.getElementById("demo").innerHTML = "Estamos probando evento en JS";
+     
+});
+
+document.getElementById('boton_color').addEventListener('click' , function () {
+     document.body.style.backgroundColor = 'red';
+});
+
+document.getElementById('boton_default').addEventListener('click' , function () {
+    document.body.style.backgroundColor = 'blue';
+});
+
+document.getElementById('boton_ocultar').addEventListener('click' , function () {
+    document.getElementById("demo").style.display = 'none';
+});
+
+const collection = document.getElementsByClassName("prueba");
+  for (let i = 0; 1 < collection.length; i++) {
+    collection[i].style.backgroundColor = "green";    
+  }
