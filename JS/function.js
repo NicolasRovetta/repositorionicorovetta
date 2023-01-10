c9d1696b677a8ff890eb58bf733a4b528737cff5
Nicolas Rovetta <nico.rovetta@gmail.com>
2023-01-09 23:18:52 -0300

    copia de seguridad

--- JS/function.js
+++ JS/function.js
@@ -3,3 +3,7 @@ document.getElementById("boton").onclick = function () {
 console.log("Capturamos el evento click");
 document.getElementById("demo").innerHTML = "Probando elemento en JS";
 }
+
+document.addEventListener("click", function()) {
+    console.log("Hola mundo desde EventListener");
+}
